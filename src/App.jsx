5c3ae1be4dabf12529abaf2beb7ee11811d7ab58
Jaydon Pearce <pearcejaydon@gmail.com>
2025-07-10 23:53:11 +1000

    Add local storage

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,56 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Ticket } from "./components/Ticket";
 import "./index.css";
 
 function App() {
-  const [totalTicketCount, setTotalTicketCount] = useState(0);
-  const [activeTickets, setActiveTickets] = useState([]);
-  const [resolvedTickets, setResolvedTickets] = useState([]);
+  const [hasLoaded, setLoaded] = useState(false);
+  const [totalTicketCount, setTotalTicketCount] = useState();
+  const [activeTickets, setActiveTickets] = useState();
+  const [resolvedTickets, setResolvedTickets] = useState();
 
-  const handleClick = () => {
+  useEffect(() => {
+    const savedActiveTickets = JSON.parse(
+      localStorage.getItem("activeTickets")
+    );
+    if (savedActiveTickets) {
+      setActiveTickets(savedActiveTickets);
+    } else {
+      setActiveTickets([]);
+    }
+
+    const savedResolvedTickets = JSON.parse(
+      localStorage.getItem("resolvedTickets")
+    );
+    if (savedResolvedTickets) {
+      setResolvedTickets(savedResolvedTickets);
+    } else {
+      setResolvedTickets([]);
+    }
+
+    const savedTotalTicketCount = JSON.parse(
+      localStorage.getItem("totalTicketCount")
+    );
+    if (savedTotalTicketCount) {
+      setTotalTicketCount(savedTotalTicketCount);
+    } else {
+      setTotalTicketCount(0);
+    }
+
+    setLoaded(true);
+  }, []);
+
+  useEffect(() => {
+    //Save to local storage on update of the active/resolved ticket count
+    if (!hasLoaded) {
+      return;
+    }
+
+    localStorage.setItem("totalTicketCount", JSON.stringify(totalTicketCount));
+    localStorage.setItem("activeTickets", JSON.stringify(activeTickets));
+    localStorage.setItem("resolvedTickets", JSON.stringify(resolvedTickets));
+  }, [activeTickets, resolvedTickets]);
+
+  const handleAddTicket = () => {
     setTotalTicketCount(totalTicketCount + 1);
 
     const newTicket = {
@@ -20,42 +63,46 @@ function App() {
     setActiveTickets([...activeTickets, newTicket]);
   };
 
-  const resolveTicket = (ticketObject) => {
+  const handleResolveTicket = (ticketObject) => {
     setActiveTickets((prev) =>
       prev.filter((ticket) => ticket.ticketNumber !== ticketObject.ticketNumber)
     );
 
-    setResolvedTickets([...resolvedTickets, ticketObject]);
+    setResolvedTickets((prev) => [...prev, ticketObject]);
   };
 
   return (
     <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <button
-          className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-          onClick={handleClick}
-        >
-          Add Ticket
-        </button>
-        <h1>Active Ticket count: {activeTickets.length}</h1>
-        <h1>Resolved Ticket count: {resolvedTickets.length}</h1>
+      {hasLoaded ? (
+        <div className="min-h-screen bg-gray-100 p-6">
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            onClick={handleAddTicket}
+          >
+            Add Ticket
+          </button>
+          <h1>Active Ticket count: {activeTickets.length}</h1>
+          <h1>Resolved Ticket count: {resolvedTickets.length}</h1>
 
-        <div
-          id="ticket-section"
-          className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4"
-        >
-          {activeTickets.map((ticket) => (
-            <Ticket
-              key={ticket.ticketNumber}
-              ticketNumber={ticket.ticketNumber}
-              raisedBy={ticket.raisedBy}
-              category={ticket.category}
-              issueDescription={ticket.issueDescription}
-              onResolve={() => resolveTicket(ticket)}
-            />
-          ))}
+          <div
+            id="ticket-section"
+            className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 mt-4"
+          >
+            {activeTickets.map((ticket) => (
+              <Ticket
+                key={ticket.ticketNumber}
+                ticketNumber={ticket.ticketNumber}
+                raisedBy={ticket.raisedBy}
+                category={ticket.category}
+                issueDescription={ticket.issueDescription}
+                onResolve={() => handleResolveTicket(ticket)}
+              />
+            ))}
+          </div>
         </div>
-      </div>
+      ) : (
+        <h1>Loading...</h1>
+      )}
     </>
   );
 }
